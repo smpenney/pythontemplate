de4aefb7e526edf19d7931ea2752cb63c53206c2
Scott Penney <scott.penney@gmail.com>
2024-06-19 09:29:07 -0400

    Name change

---
+++ unit_tests/test_base/test_base_2.py
@@ -0,0 +1,13 @@
+import pytest
+
+from pythontemplate.common import BaseLearner  # pylint: disable=wrong-import-position, disable=import-error
+
+
+@pytest.mark.unit
+def test_base():
+    """Unit test for the BaseLearner class
+    """
+    print("test_base")
+
+    base = BaseLearner()
+    assert base.test() == "test"
